eb53ecb84f470196213e5f4689cc4fc49f1c82f0
NehamaMandelbaum <nehama1836@gmail.com>
2021-02-18 19:19:35 -0300

    aula ejs

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,19 @@ const model = require ('../model/products')
 const router = express.Router()
 
 router.get('/', function(req, res) {
-    res.render('products', {
-        title: 'Página de produtos legalsíssima',
-        products: model.getProducts()
-    })
+    const types = [{
+        id: 'electronic',
+        label: 'Eletrônico'
+    }, {
+        id: 'food', 
+        label: 'Alimentício'
+    } , {
+        id: 'drugs',
+        label: 'Farmacêutico'
+    }]
+    res.render('products', {name: 'Pagina de Produtos', types: types})
 })
 
 module.exports = router
 
+ 
